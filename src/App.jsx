0d624ab206dbc84517947b1f64d42a067c56cf5c
Valentin Meyer <valenmmm1@gmail.com>
2024-07-29 19:00:41 -0300

    Correccion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import Navbar from './components/Navbar/Navbar.jsx'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
 
 function App() {
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css"
-import CartWidget from "../CartWidget/CartWidget"
+import CartWidget from "../CartWidget/CartWidget.jsx"
 
 const Navbar = () => {
 return (
