303a616c829f73cb53b800f707f0bb8529221087
Valentin Meyer <valenmmm1@gmail.com>
2024-08-25 00:24:13 -0300

    Arreglos en las imagenes

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,58 +2,62 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 
+
 import blackLabel from '../assets/blackLabel.webp';
 import jackDaniels from '../assets/jackDaniels.webp';
-import macallanRare from '../assets/macallanRare.webp';
-import glenfiddich18 from '../assets/glenfiddich.18.webp';
-import vat01 from '../assets/vat.01.webp';
+import macallanSherryOak from '../assets/12.3.webp';
+import glenfiddichSoleraReserve from '../assets/15años.webp';
+import chivas12 from '../assets/Chivas12años.webp';
 import blueLabel from '../assets/blueLabel.webp';
 import jackSingle from '../assets/jackSingle.webp';
-import macallanDouble from '../assets/18.1.webp';
-import vat02 from '../assets/vat.02.webp';
+import macallanDoubleCask from '../assets/18.1.webp';
+import glenfiddich18 from '../assets/glenfiddich.18.webp';
+import chivasExtra from '../assets/ChivasExtra.webp';
 import goldLabel from '../assets/goldLabel.webp';
 import jackHoney from '../assets/jackHoney.webp';
 import macallanRareCask from '../assets/macallanRare.webp';
 import glenfiddich21 from '../assets/21años.webp';
-import chivas15 from '../assets/15años.webp';
+import chivas18 from '../assets/chivasRegal18.webp';
 import redLabel from '../assets/redLabel.webp';
 import gentlemanJack from '../assets/Gentleman.webp';
-import macallanFineOak from '../assets/12.3.webp';
-import fireAndCane from '../assets/12años.webp';
-import chivasUltis from '../assets/12años.webp';
+import macallanFineOak from '../assets/macallanFine.webp';
+import glenfiddichFire from '../assets/glenfiddichFire.webp';
+import chivasUltis from '../assets/chivasRegalUltis.webp';
 import greenLabel from '../assets/greenLabel.webp';
-import jackApple from '../assets/jackApple.webp';
-import macallanEdition from '../assets/12.2.webp';
-import ipaExperiment from '../assets/15años.webp';
-import mizunara from '../assets/12años.webp';
-import swing from '../assets/dobleBlack.webp';
+import jackSinatra from '../assets/jackDanielsSinatra.webp';
+import macallanEdition6 from '../assets/macallanEdition6.webp';
+import glenfiddichIpa from '../assets/glenfiddichIpa.webp';
+import chivasMizunara from '../assets/chivasRegalMizura.webp';
+import swing from '../assets/Swing.webp';
+
 
 const imageMap = {
-    "./blackLabel.webp": blackLabel,
-    "./jackDaniels.webp": jackDaniels,
-    "./macallanRare.webp": macallanRare,
-    "./glenfiddich.18.webp": glenfiddich18,
-    "./vat.01.webp": vat01,
-    "./blueLabel.webp": blueLabel,
-    "./jackSingle.webp": jackSingle,
-    "./18.1.webp": macallanDouble,
-    "./vat.02.webp": vat02,
-    "./goldLabel.webp": goldLabel,
-    "./jackHoney.webp": jackHoney,
-    "./macallanRare.webp": macallanRareCask,
-    "./21años.webp": glenfiddich21,
-    "./15años.webp": chivas15,
-    "./redLabel.webp": redLabel,
-    "./Gentleman.webp": gentlemanJack,
-    "./12.3.webp": macallanFineOak,
-    "./12años.webp": fireAndCane,
-    "./12años.webp": chivasUltis,
-    "./greenLabel.webp": greenLabel,
-    "./jackApple.webp": jackApple,
-    "./12.2.webp": macallanEdition,
-    "./15años.webp": ipaExperiment,
-    "./12años.webp": mizunara,
-    "./dobleBlack.webp": swing
+"./blackLabel.webp": blackLabel,
+"./jackDaniels.webp": jackDaniels,
+"./12.3.webp": macallanSherryOak,
+"./15años.webp": glenfiddichSoleraReserve,
+"./Chivas12años.webp": chivas12,
+"./blueLabel.webp": blueLabel,
+"./jackSingle.webp": jackSingle,
+"./18.1.webp": macallanDoubleCask,
+"./glenfiddich.18.webp": glenfiddich18,
+"./ChivasExtra.webp": chivasExtra,
+"./goldLabel.webp": goldLabel,
+"./jackHoney.webp": jackHoney,
+"./macallanRare.webp": macallanRareCask,
+"./21años.webp": glenfiddich21,
+"./chivasRegal18.webp": chivas18,
+"./redLabel.webp": redLabel,
+"./Gentleman.webp": gentlemanJack,
+"./macallanFine.webp": macallanFineOak,
+"./glenfiddichFire.webp": glenfiddichFire,
+"./chivasRegalUltis.webp": chivasUltis,
+"./greenLabel.webp": greenLabel,
+"./jackDanielsSinatra.webp": jackSinatra,
+"./macallanEdition6.webp": macallanEdition6,
+"./glenfiddichIpa.webp": glenfiddichIpa,
+"./chivasRegalMizura.webp": chivasMizunara,
+"./Swing.webp": swing
 };
 
 const ProductItem = ({ id, image, name, price }) => {
